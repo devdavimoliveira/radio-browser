fceee510c47d905c0599086691ba63132d7dccf0
Davi Machado Oliveira <devdavimoliveira@gmail.com>
2025-02-16 00:54:40 -0300

    chore: create Station type

---
+++ src/@types/station.ts
@@ -0,0 +1,15 @@
+export type Station = {
+  stationuuid: string
+  name: string
+  url: string
+  url_resolved: string
+  homepage: string
+  favicon: string
+  tags: string
+  country: string
+  countrycode: string
+  language: string
+  languagecodes: string
+  votes: number
+  clickcount: number
+}
